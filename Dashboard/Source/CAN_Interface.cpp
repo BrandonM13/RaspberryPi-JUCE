b62b054d00cb2923745d29e76743fe432007195d
BrandonM13 <brandonmunford13@gmail.com>
2021-10-22 09:12:52 +1300

    thread stuff

--- Dashboard/Source/CAN_Interface.cpp
+++ Dashboard/Source/CAN_Interface.cpp
@@ -33,6 +33,12 @@ void CAN_Interface::run() {
 }
 
 int CAN_Interface::getId() { return (frame.can_id << 1) >> 1; }
-int CAN_Interface::readData(int idx) { return frame.data[idx]; }
+int CAN_Interface::readData(int idx) {
+    
+    return (frame.data[idx] << 8) + frame.data[idx + 1];
+
+    //return frame.data[idx];
+
+}
 
 void CAN_Interface::readCAN() { read(s, &frame, sizeof(struct can_frame)); }
